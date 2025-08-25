eca345ef845ff2f036642f5d575a47b255c981a5
victorma0123 <victor.ma.liangchun@gmail.com>
2025-08-25 10:36:56 +0800

    更新第2，3本地

--- 03_strands_tooluse/strands-tooluse.py
+++ 03_strands_tooluse/strands-tooluse.py
@@ -1,6 +1,10 @@
 from strands import Agent, tool
 from strands_tools import calculator
-
+from strands.models.ollama import OllamaModel
+model = OllamaModel(
+    host="http://127.0.0.1:11434",
+    model_id="qwen3:1.7b"   # 换成你机器上实际有的模型（ollama list）
+)
 @tool
 def weather(city: str) -> str:
     """Get weather information for a city
@@ -14,13 +18,14 @@ def weather(city: str) -> str:
 
 # Initialize your agent
 agent = Agent(
+    model=model,
     tools=[weather, calculator],
     system_prompt="You are a helpful assistant that provides concise responses.",
     callback_handler=None
 )
 
 print("\nTool Config:\n")
-print(agent.tool_config)
+#print(agent.tool)
 
 while True:
     try:
